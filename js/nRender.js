ed27620d08ce1a88e2d0db2edc65bc5bf0b3df0c
Liav Koren <liav.koren+gitHub@gee-mail.com>
2013-07-10 23:42:17 -0400

    See film_edge_bloom1.jpg. Also a very blurry post effect.

--- js/nRender.js
+++ js/nRender.js
@@ -9,7 +9,7 @@ animate();
 function initCamScene() {
 	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
 	camera.position.z = 20;
-	scene.fog = new THREE.FogExp2( 0x202020, 0.2 );
+	//scene.fog = new THREE.FogExp2( 0x202020, 0.2 );
 }
 function initRenderer() {
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -33,25 +33,68 @@ function initRenderer() {
 ////
 	//renderer.autoClear = false;
 	//renderer.sortObjects = false;
-	var renderModel = new THREE.RenderPass( scene, camera );
-	var effectBloom = new THREE.BloomPass( 20 );
-	var effectFilm = new THREE.FilmPass( 1, .1, 5000, false );
-	effectFocus = new THREE.ShaderPass( THREE.FocusShader );
-	effectFocus.uniforms[ "screenWidth" ].value = window.innerWidth;
-	effectFocus.uniforms[ "screenHeight" ].value = window.innerHeight;
-	effectFocus.renderToScreen = true;
-
+	var renderModel = new THREE.RenderPass( scene, camera );	
 	composer = new THREE.EffectComposer( renderer );
-	composer.addPass( renderModel );
-	composer.addPass( effectBloom );
+	composer.addPass( renderModel );	
+
+	var rgbShiftEffect = new THREE.ShaderPass( THREE.RGBShiftShader );
+	rgbShiftEffect.uniforms[ 'amount' ].value = 0.0015;
+	//composer.addPass( rgbShiftEffect );
+
+
+
+	var effectFilm = new THREE.FilmPass( 0.1, 0.0, 0.0, false );
 	composer.addPass( effectFilm );
-	composer.addPass( effectFocus );	
+	
+var edgeEffect = new THREE.ShaderPass( THREE.EdgeShader );
+	edgeEffect.uniforms[ 'aspect' ].value.x = window.innerWidth;
+	edgeEffect.uniforms[ 'aspect' ].value.y = window.innerHeight;
+	composer.addPass( edgeEffect );
+
+	var effectBloom = new THREE.BloomPass( 3, 25, 10, 512);
+	//filmShader + edgeShader + bloom(10,25,10) gives a nice, blown-out, fuzzy effect.
+	//bloom interface: ( strength, kernelSize, sigma, resolution )
+	//strength: how strong is the effect. 
+	//resoluation a quality setting. Above 1024, gets too expensive. 
+	//kernalSize: kernal size is a sort of resolution setting. The signal is probably being convolved with a kernal, 
+		//and this setting affects that. Also seems computationally expensive. Keep < 40, ideally less than 10.  
+	//sigma: a sort of bleed or blur setting. 
+	//seems to affect how blurry/sharp the output is. Higher = sharper
+
+	/*	strength = ( strength !== undefined ) ? strength : 1;
+	kernelSize = ( kernelSize !== undefined ) ? kernelSize : 25;
+	sigma = ( sigma !== undefined ) ? sigma : 4.0;
+	resolution = ( resolution !== undefined ) ? resolution : 256;*/
+	composer.addPass( effectBloom ); 
+
+	
+
+	var effect = new THREE.ShaderPass( THREE.CopyShader);
+	effect.renderToScreen = true;
+	composer.addPass( effect );
+
+
+	//effectBloom.renderToScreen = true;
+	
+
+	//effectFocus = new THREE.ShaderPass( THREE.FocusShader );
+	//effectFocus.uniforms[ "screenWidth" ].value = window.innerWidth;
+	//effectFocus.uniforms[ "screenHeight" ].value = window.innerHeight;
+	//effectFocus.renderToScreen = true;
+
+
+	
+	//composer.addPass( effectFilm );
+	//composer.addPass( effectFocus );	
 }
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	render();
+
+	edgeEffect.uniforms[ 'aspect' ].value.x = window.innerWidth;
+	edgeEffect.uniforms[ 'aspect' ].value.y = window.innerHeight;	
 }
 function animate() {
 	requestAnimationFrame( animate );
@@ -68,5 +111,5 @@ function render() {
 	}	
 	//renderer.render(scene, camera);
 	renderer.clear();
-	composer.render( 0.01 );	
+	composer.render();	
 }
